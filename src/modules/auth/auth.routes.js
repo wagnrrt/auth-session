8073ed4c2008a2c42608b1fd72c2e4175a3fe01f
wagnrrt <wagnrb0@gmail.com>
2026-04-15 11:38:38 -0300

    refactor: update hook import

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,8 +1,8 @@
-import { authenticate } from "../../hooks/auth.hook.js"
 import { signIn, signUp, signOut } from "./auth.controller.js"
+import { requireAuth } from "../../hooks/require-auth.js"
 
 export async function authRoutes(app) {
   app.post('/auth/sign-in', signIn)
   app.post('/auth/sign-up', signUp)
-  app.post('/auth/sign-out', { preHandler: authenticate }, signOut)
+  app.post('/auth/sign-out', { preHandler: requireAuth }, signOut)
 }
