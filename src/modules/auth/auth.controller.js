9caf7253f0153d33c9211938b67fcb86880d5606
wagnrrt <wagnrb0@gmail.com>
2026-04-15 11:38:08 -0300

    refactor: extract setSessionCookie helper

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,59 +1,53 @@
-import { signInSchema, signUpSchema } from "../../schemas/auth.schema.js"
-import { parseUserAgent } from "../../utils/user-agent.parser.js";
-import * as authService from "./auth.service.js";
+import { signInRequestSchema, signUpRequestSchema } from "../../schemas/auth.schema.js"
+import { parseUserAgent } from "../../utils/user-agent.parser.js"
+import * as authService from "./auth.service.js"
+
+const COOKIE_OPTIONS = {
+  signed: true,
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'strict',
+  path: '/',
+}
+
+function setSessionCookie(reply, token) {
+  reply.setCookie('session', token, {
+    ...COOKIE_OPTIONS,
+    maxAge: 7 * 24 * 60 * 60 * 1000
+  })
+}
 
 export async function signUp(request, reply) {
-  const data = signUpSchema.parse(request.body)
+  const data = signUpRequestSchema.parse(request.body)
   const ua = parseUserAgent(request.headers['user-agent'])
 
-  const session = await authService.signUp(request.server.db, {
-    name: data.name,
-    email: data.email,
-    password: data.password,
+  const token = await authService.signUp(request.server.db, {
+    ...data,
     ipAddress: request.ip,
-    os: ua.os,
-    browser: ua.browser,
-    device: ua.device,
+    ...ua
   })
 
-  reply.setCookie('session', session, {
-    signed: true,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: 'strict'
-  })
-
-  return reply.code(201).send({ message: 'signUp successful' })
+  setSessionCookie(reply, token)
+  return reply.code(201).send({ message: 'User registered successfully' })
 }
 
 export async function signIn(request, reply) {
-  const data = signInSchema.parse(request.body)
+  const data = signInRequestSchema.parse(request.body)
   const ua = parseUserAgent(request.headers['user-agent'])
 
-  const session = await authService.signIn(request.server.db, {
-    email: data.email,
-    password: data.password,
+  const token = await authService.signIn(request.server.db, {
+    ...data,
     ipAddress: request.ip,
-    os: ua.os,
-    browser: ua.browser,
-    device: ua.device,
+    ...ua
   })
 
-  reply.setCookie('session', session, {
-    signed: true,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: 'strict'
-  })
-
-  return reply.code(201).send({ message: 'signIn successful' })
+  setSessionCookie(reply, token)
+  return reply.code(200).send({ message: 'User logged in successfully' })
 }
 
 export async function signOut(request, reply) {
   console.log(request.user)
   await authService.signOut(request.server.db, request.user.sessionId)
   reply.clearCookie('session')
-  return reply.code(201).send({ message: 'signOut successful' })
+  return reply.code(200).send({ message: 'User logged out successfully' })
 }
