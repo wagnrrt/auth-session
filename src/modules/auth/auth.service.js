70c91a4907badb6e9c352ee54b2ada759938b620
wagnrrt <wagnrb0@gmail.com>
2026-04-15 11:35:08 -0300

    refactor: extract generateSession and remove duplication

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,67 +1,60 @@
 import bcrypt from 'bcrypt'
-import { transaction } from '../../plugin/db.js';
+import { transaction } from '../../plugin/db.js'
 import { v7 as uuidv7 } from 'uuid'
-import { hashToken } from '../../utils/hash.js';
-import { BaseError } from '../../errors/base-error.js';
+import { hashToken } from '../../utils/hash.js'
+import { BaseError } from '../../errors/base-error.js'
 import { randomBytes } from 'crypto'
-import * as authRepository from "./auth.repository.js";
+import * as authRepository from "./auth.repository.js"
 
-export async function signUp(db, data) {
-  const userId = uuidv7()
+
+async function generateSession(db, data) {
   const sessionId = uuidv7()
-  const hash = await bcrypt.hash(data.password, 10)
   const token = randomBytes(32).toString('hex')
 
-  await transaction(db, async (c) => {
-    await authRepository.insertUser(c, {
-      id: userId,
-      name: data.name,
-      email: data.email,
-    });
-
-    await authRepository.insertCredential(c, {
-      userId,
-      password: hash
-    });
-
-    await authRepository.insertSession(c, {
-      id: sessionId,
-      userId,
-      token: hashToken(token),
-      expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-      ipAddress: data.ipAddress,
-      os: data.os,
-      browser: data.browser,
-      device: data.device
-    })
-  });
+  await authRepository.insertSession(db, {
+    id: sessionId,
+    token: hashToken(token),
+    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    ...data
+  })
 
   return token
 }
 
+export async function signUp(db, data) {
+  const userId = uuidv7()
+  const hash = await bcrypt.hash(data.password, 10)
+
+  return await transaction(db, async (c) => {
+    await authRepository.insertUser(c, {
+      id: userId,
+      name: data.name,
+      email: data.email
+    })
+
+    await authRepository.insertCredential(c, {
+      userId,
+      password: hash
+    })
+
+    return await generateSession(c, { userId, ...data })
+  })
+}
+
 export async function signIn(db, data) {
-  const sessionId = uuidv7()
-  const token = randomBytes(32).toString('hex')
   const authData = await authRepository.findAuthDataByEmail(db, data.email)
   if (!authData) throw new BaseError('invalid credentials', 401)
 
   if (!(await bcrypt.compare(data.password, authData.password)))
     throw new BaseError('invalid credentials', 401)
 
-  await authRepository.insertSession(db, {
-    id: sessionId,
-    userId: authData.id,
-    token: hashToken(token),
-    expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    ipAddress: data.ipAddress,
-    os: data.os,
-    browser: data.browser,
-    device: data.device
-  })
-
-  return token
+  return await generateSession(db, { userId: authData.id, ...data })
 }
 
 export async function signOut(db, sessionId) {
   await authRepository.deleteSessionById(db, sessionId)
+}
+
+export async function validateSessionToken(db, token) {
+  return await authRepository.findSessionByToken(db, hashToken(token))
 }
