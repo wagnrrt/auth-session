ba39836f90bdd91938fc54b98eece289542545c9
wagnrrt <wagnrb0@gmail.com>
2026-04-15 11:34:15 -0300

    refactor: rename auth schemas with  Request suffix

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,12 +1,12 @@
 import z from "zod"
 
-export const signUpSchema = z.object({
+export const signUpRequestSchema = z.object({
   name: z.string().min(2).max(255),
   email: z.email().max(255),
   password: z.string().min(8).max(255)
 })
 
-export const signInSchema = z.object({
+export const signInRequestSchema = z.object({
   email: z.email().max(255),
   password: z.string().min(8).max(255)
 })
