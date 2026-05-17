147d1917300ba1f53e27161a9761fdc2208b229d
wagnrrt <wagnrb0@gmail.com>
2026-05-17 20:37:55 +0000

    chore: migrate project setup to bun

--- src/modules/auth/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { signIn, signUp, signOut } from "./auth.controller.js"
-import { requireAuth } from "../../hooks/require-auth.js"
-
-export async function authRoutes(app) {
-  app.post('/auth/sign-in', signIn)
-  app.post('/auth/sign-up', signUp)
-  app.post('/auth/sign-out', { preHandler: requireAuth }, signOut)
-}
