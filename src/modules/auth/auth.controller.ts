2bb934ed49c3a5b5234876eb0769f8ef7d3ed38b
wagnrrt <wagnrb0@gmail.com>
2026-05-27 16:24:09 +0000

    feat: add signIn handler

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,3 +1,5 @@
-export async function signIn() {
+import type { FastifyReply, FastifyRequest } from "fastify";
 
+export async function signIn(request: FastifyRequest, reply: FastifyReply) {
+  return reply.code(200).send({ message: 'user logged in successfully' })
 }
